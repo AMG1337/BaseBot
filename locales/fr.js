feb164c0fe75bc9aefc7fd3a9d8e2f23d67e7083
AMG1337 <karimlip2563@hotmail.com>
2025-03-31 04:31:44 +0200

    Update fr.js

--- locales/fr.js
+++ locales/fr.js
@@ -1,1 +1,30 @@
-d
+module.exports = {
+    name: "Français",
+    dictionnary: {
+      mentionbot: (name) => {
+        return `Salut **${name}** utilise la commande \`/help\` pour découvrir mes commandes !`
+      },
+      cooldown: (time) => {
+        return `**Veuillez attendre** \`${time}\` **avant de réutiliser cette commande !**`;
+      },
+      pingCommand : {
+        title: "Latence de",
+        ping: (client, api) => {
+          return `**🚀・Client:** \`${client} ms\`\n\n**💻・API:** \`${api} ms\``
+        },
+        wait: "**Calcul en cours...**"
+      },
+      AddBot : {
+        title: (name) => {
+          return `Ajoute ${name} sur ton serveur !`
+        },
+        description: "🔎・*Clique sur le bouton ci-dessous pour m'ajouter à un serveur*",
+        bouton: "Clique ici !",
+      },
+  
+      autres: {
+        demandepar: "Demandé par:"
+      },
+    },
+  };
+  
