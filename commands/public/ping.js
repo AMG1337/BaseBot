095b7ae81544b75dd8b975e4309dc8ddd4ac3401
AMG1337 <karimlip2563@hotmail.com>
2025-03-31 04:30:15 +0200

    Create ping.js

---
+++ commands/public/ping.js
@@ -0,0 +1,32 @@
+const { EmbedBuilder } = require("discord.js");
+
+module.exports = {
+    data: {
+        name: 'ws',
+        description: 'Show the ping of the bot',
+        aliases: ["ping"],
+        usage: "",
+        userPermissions: [""],
+        clientPermissions: ["SendMessages", "EmbedLinks", "AttachFiles"],
+        cooldown: 2000,
+    },
+    run: async (client, message, args) => {
+
+        const [guildRows] = await client.db.query("SELECT * FROM guilds WHERE guildId = ?", [message.guild.id]);
+        guildData = guildRows
+
+        const msg = await message.reply({ content: client.LangHandler.get(guildData.lang).pingCommand.wait, fetchReply: true, ephemeral: true });
+        const ms = msg.createdTimestamp - message.createdTimestamp
+
+        const embed = new EmbedBuilder()
+        .setColor("#2e2e34")
+        .setTitle(`${client.LangHandler.get(guildData.lang).pingCommand.title} ${client.user.username}`)
+        .setDescription(`${client.LangHandler.get(guildData.lang).pingCommand.ping(ms, client.ws.ping)}`)
+        .setFooter({
+            text: `${client.config.name}・${client.LangHandler.get(guildData.lang).autres.demandepar} ${message.author.globalName }・${client.functions.getFormattedTime()}`,
+            iconURL: client.user.displayAvatarURL({ dynamic: true })
+        })
+
+        return msg.edit({ content: "", embeds: [embed] });
+    }
+}
